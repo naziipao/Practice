d241282c63e4dee97922610ecbb292d145f4186f
nazif <nazifahmedrifat@gmail.com>
2025-01-21 02:45:15 +0600

    commit

---
+++ src/Tester.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Tester {
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int [][] arr1 = new int[t][3];
+
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            arr1[i][0] = a;
+            arr1[i][1] = b;
+            arr1[i][2] = n;
+        }
+
+        int num = 0;
+        for(int i = 0; i <t ; i++) {
+            //Calculating each query
+
+            for (int j = 0; j<arr1[i][2];j++) {
+                num = num + (int) Math.pow(2, j);
+                System.out.print((arr1[i][0]+num * arr1[i][1])+" ");
+            }
+
+            System.out.println();
+            num = 0;   //Resetting the value of num for next query
+        }
+    }
+}
